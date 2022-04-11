eeb9391eb1513bf1c7678accec76c094dfdd57d9
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-11 12:18:36 -0300

    feat: add migration of create categories table

---
+++ src/infra/data/mysql/migrations/1649689989627-CreateCategories.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategories1649689989627 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'current_timestamp',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'current_timestamp',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
