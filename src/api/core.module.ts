3216b1973e4bc1b64d218aa622516a12e2bef7d2
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-14 06:57:40 -0300

    feat: remove core module

--- src/api/core.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AnnouncementsModule } from './announcements.module';
-import { CategoriesModule } from './categories.module';
-import { UsersModule } from './users.module';
-
-@Module({
-  imports: [AnnouncementsModule, CategoriesModule, UsersModule],
-})
-export class CoreModule {}
