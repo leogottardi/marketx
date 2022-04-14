f82d23948905ddd62d7077b0ff1f105beaec6878
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-14 06:57:01 -0300

    feat: add categories api

---
+++ src/api/categories/categories.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesService } from 'src/app/categories/categories.service';
+import { Category } from 'src/domain/categoy/entities/category.entity';
+import { CategoriesRepository } from 'src/infra/data/mysql/repository/implementations/categories.repository';
+import { CategoriesController } from './categories.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Category])],
+  controllers: [CategoriesController],
+  providers: [
+    CategoriesService,
+    {
+      provide: 'CategoriesRepository',
+      useClass: CategoriesRepository,
+    },
+  ],
+})
+export class CategoriesModule {}
