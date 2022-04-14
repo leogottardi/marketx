28a600c17314c05a5fd7b4d8105592073eddddf8
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-14 10:41:14 -0300

    feat: add bcrypt to hash user password

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ICreateUserDTO } from 'src/domain/user/dto/create-user.dto';
 import { User } from 'src/domain/user/entities/user.entity';
 import { IUsersRepository } from 'src/infra/data/mysql/repository/IUsersRepository';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -11,6 +12,13 @@ export class UsersService {
   ) {}
 
   async create(data: ICreateUserDTO): Promise<User> {
-    return await this.usersRepository.create(data);
+    const { password } = data;
+
+    const password_hash = await hash(password, 8);
+
+    return await this.usersRepository.create({
+      ...data,
+      password: password_hash,
+    });
   }
 }
