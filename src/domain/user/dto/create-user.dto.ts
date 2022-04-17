f17a3a9a9d228b0134157e1df80f85bce8b9ccf3
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-17 20:29:04 -0300

    feat: add validator on create user dto

--- src/domain/user/dto/create-user.dto.ts
+++ src/domain/user/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
-export interface ICreateUserDTO {
+import { IsEmail, IsNotEmpty, Length, Min } from 'class-validator';
+
+export class ICreateUserDTO {
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
+  @Length(6)
   password: string;
+
+  @IsNotEmpty()
   cpf: string;
 }
